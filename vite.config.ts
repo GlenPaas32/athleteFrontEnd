946e465a50be6323132be3f1cbe47fdf94410649
GlenPaas32 <glenpaas@tlu.ee>
2026-05-08 18:08:57 +0300

    VITE

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/athletes': 'http://localhost:8080',
-      '/api': 'http://localhost:8080',
+export default defineConfig(({ mode }) => {
+  const env = {
+    ...loadEnv('dev', process.cwd(), ''),
+    ...loadEnv(mode, process.cwd(), ''),
+  }
+  const backendUrl = env.VITE_BACK_URL || 'http://localhost:8080'
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/athletes': backendUrl,
+        '/api': backendUrl,
+      },
     },
-  },
+  }
 })
